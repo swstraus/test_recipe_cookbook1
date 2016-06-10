0fdeeb22930a0451ee5e29e8a45e1d4e1958b62a
Steve Straus <steve.straus@teamaol.com>
2016-06-10 12:24:22 -0400

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: test_recipe_cookbook1
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+base_test_instance "test_tomcat" do 
+   serverBase "/data/servers"
+end
